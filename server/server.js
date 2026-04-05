d29b437b1af3b9869002fd671eb7d5a0173f5cc8
Amirvil <amir.vilensky@gmail.com>
2026-04-05 12:03:38 +0300

    Test Server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,38 @@
 import express from 'express'
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+
+const bugs = [
+    {
+        title: "Infinite Loop Detected",
+        severity: 4,
+        _id: "1NF1N1T3"
+    },
+    {
+        title: "Keyboard Not Found",
+        severity: 3,
+        _id: "K3YB0RD"
+    },
+    {
+        title: "404 Coffee Not Found",
+        severity: 2,
+        _id: "C0FF33"
+    },
+    {
+        title: "Unexpected Response",
+        severity: 1,
+        _id: "G0053"
+    }
+]
+
+
+
+
+app.get('/api/bug', (req, res) => {
+    res.send(bugs)
+})
+
+app.get('/test', (req, res) => {
+    res.send("The server is definitely working!")
+})
+
+app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030'))
