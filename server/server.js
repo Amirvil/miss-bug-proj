71f73bbc7b136cfd7c3b7fe514ed5c661404f659
Amirvil <amir.vilensky@gmail.com>
2026-04-05 19:27:34 +0300

    Add Save

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { makeId } from '../services/util.service.js'
 const app = express()
 
 const bugs = [
@@ -31,7 +32,24 @@ app.get('/api/bug', (req, res) => {
     res.send(bugs)
 })
 
-app.get('/api/bug/save', (req, res) => { })
+app.get('/api/bug/save', (req, res) => {
+    const { title, severity, bugId } = req.query
+    const bugToSave = {
+        title,
+        severity: +severity,
+        _id: bugId
+    }
+
+    if (bugId) {
+        const idx = bugs.findIndex(bug => bug._id === bugId)
+        bugs[idx] = { ...bugs[idx], ...bugToSave }
+    } else {
+        bugToSave._id = makeId()
+        bugs.push(bugToSave)
+    }
+
+    res.send(bugToSave)
+})
 
 app.get('/api/bug/:bugId', (req, res) => {
     const bugId = req.params.bugId
@@ -45,6 +63,6 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
     bugs.splice(idx, 1)
 
     res.send('Bug Deleted')
- })
+})
 
 app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030'))
