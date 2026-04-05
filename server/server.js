be16a8076144e42097e72917bf2506d947b20d94
Amirvil <amir.vilensky@gmail.com>
2026-04-05 19:11:51 +0300

    Add remove and bugId

--- server/server.js
+++ server/server.js
@@ -31,8 +31,20 @@ app.get('/api/bug', (req, res) => {
     res.send(bugs)
 })
 
-app.get('/test', (req, res) => {
-    res.send("The server is definitely working!")
+app.get('/api/bug/save', (req, res) => { })
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const bugId = req.params.bugId
+    const bug = bugs.find(bug => bug._id === bugId)
+    res.send(bug)
 })
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const bugId = req.params.bugId
+    const idx = bugs.findIndex(bug => bug._id === bugId)
+    bugs.splice(idx, 1)
+
+    res.send('Bug Deleted')
+ })
 
 app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030'))
